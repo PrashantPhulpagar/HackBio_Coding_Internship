6d35ea002a0bf18e26e7a88ae7f2f63f47229458
Prashant Phulpagar <ptphulpagar@gmail.com>
2025-02-06 23:49:28 +0530

    Add files via upload

---
+++ stage-zero/stage-zero_task.py
@@ -0,0 +1,19 @@
+personal_profile = {
+    "name": "Prashant Phulpagar",
+    "slack_username": "@Prashant",
+    "email": "prashantphulpagar0@gmail.com",
+    "hobby": "Playing Badminton",
+    "country": "India",
+    "discipline": "Genetics and Genomics",
+    "preferred_language": "Python"
+}
+
+print("Personal Profile:")
+print(f"Name: {personal_profile['name']}")
+print(f"Slack Username: {personal_profile['slack_username']}")
+print(f"Email: {personal_profile['email']}")
+print(f"Hobby: {personal_profile['hobby']}")
+print(f"Country: {personal_profile['country']}")
+print(f"Discipline: {personal_profile['discipline']}")
+print(f"Preferred Programming Language: {personal_profile['preferred_language']}") 
+
